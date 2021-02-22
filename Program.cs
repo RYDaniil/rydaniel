205fc4cf01c20295b7b9f954af875c27c5815edd
RYDaniil <79157317+RYDaniil@users.noreply.github.com>
2021-02-22 19:58:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Vvedite:  ");
+			int[] a = new int[8];
+			for (int i = 0; i < 8; i++)
+			{
+				a[i] = int.Parse(Console.ReadLine());
+			}
+			int[] b = new int[8];
+			Console.WriteLine("Otvet:  ");
+			for (int i = 0; i < 8; i++)
+			{
+				b[i] = a[i] % 10;
+				Console.WriteLine(b[i]);
+			}
+		}
+    }
+}
